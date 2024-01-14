876cd6d05ecdad1c4fcb98fa5d1399f6df439064
nestorhugo <nestorhugo@proton.me>
2024-01-13 22:40:25 -0300

    added 'favorites' services

---
+++ services/favorito.js
@@ -0,0 +1,35 @@
+import getTodosLivros from "./livro.js";
+
+const fs = require("fs");
+
+function getTodosFavoritos() {
+  return JSON.parse(fs.readFileSync("favoritos.json"));
+}
+
+function deletaFavoritoPorId(id) {
+  const favoritos = getTodosFavoritos();
+  const favorito = favoritos.find((favorito) => favorito.id === id);
+  if (!favorito) {
+    throw new Error("Favorito não encontrado");
+  }
+  const favoritosAtualizados = favoritos.filter(
+    (favorito) => favorito.id !== id
+  );
+  fs.writeFileSync("favoritos.json", JSON.stringify(favoritosAtualizados));
+}
+
+function insereFavorito(id) {
+  const livros = getTodosLivros();
+  const favoritos = getTodosFavoritos();
+
+  const livroInserido = livros.find((livro) => livro.id === id);
+  const novaListaFavoritos = [...favoritos, livroInserido];
+
+  fs.writeFileSync("favoritos.json", JSON.stringify(novaListaFavoritos));
+}
+
+module.exports = {
+  getTodosFavoritos,
+  deletaFavoritoPorId,
+  insereFavorito,
+};
